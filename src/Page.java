efd3862e0a61a9ed4a478721b506a281877c5df3
Sai Madhu Bhargav <mbhargav294@gmail.com>
2017-04-18 23:19:13 -0500

    Almost finished. Implemented the PageRank Algorithm

--- src/Page.java
+++ src/Page.java
@@ -5,28 +5,58 @@ import java.util.*;
  */
 
 /**
- * @author mbhar
+ * @author vyshnavi_bandi
  *
  */
-public class Page {
+public class Page implements Comparable<Page>{
 	String text;
 	int docNo;
+	double oldRankScore;
+	double newRankScore;
+	int numberOfLinks;
 	Set<String> words;
+	Set<Page> links;
 	
 	Page(int d, String t, Set<String> stopWords)
 	{
 		text = t;
 		docNo = d;
+		oldRankScore = 1.0;
+		newRankScore = 1.0;
+		numberOfLinks = 0;
 		words  = new HashSet<>();
+		links = new HashSet<>();
 		
 		String[] wordsInText = text.toLowerCase().replaceAll("[^a-z0-9 ]", " ").split(" ");
 		for(String word : wordsInText)
-			if(word.length() > 0 && !stopWords.contains(word))
+			if(word.length() > 1 && !stopWords.contains(word))
 				words.add(word);
-		
-		//System.out.print(docNo + "->");
-		//for(String word : words)
-		//	System.out.print(word + " ");
-		//System.out.println();
+	}
+	
+	void calculateScore(Set<Page> links)
+	{
+		this.links = links;
+		numberOfLinks = links.size();
+		for(Page p : links)
+		{
+			p.newRankScore += oldRankScore/(double)numberOfLinks;
+		}
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return this.docNo;
+	}
+
+	@Override
+	public int compareTo(Page p) {
+		double x = this.newRankScore - p.newRankScore;
+		if(x < 0)
+			return 1;
+		else if(x == 0)
+			return 0;
+		else
+			return -1;
 	}
 }
--- src/pageRank.java
+++ src/pageRank.java
@@ -6,7 +6,7 @@ import java.util.*;
  */
 
 /**
- * @author mbhar
+ * @author madhu_bhargav
  *
  */
 public class pageRank {
@@ -19,7 +19,8 @@ public class pageRank {
 		File f = new File("./stops.txt");
 		Scanner scan = new Scanner(f);
 		Set<String> stopWords = new HashSet<>();
-		Map<Integer, Page> documents = new HashMap<>();
+		Map<Integer, Page> documents = new TreeMap<>();
+		Map<String, Set<Page>> wordLinks = new HashMap<>();
 		
 		while(scan.hasNext())
 			stopWords.add(scan.next().toLowerCase());
@@ -30,7 +31,57 @@ public class pageRank {
 		while(scan.hasNextLine())
 		{
 			Page page = new Page(docNo, scan.nextLine(), stopWords);
-			documents.put(docNo++, page);
+			documents.put(docNo, page);
+			
+			for(String word : page.words)
+			{
+				if(wordLinks.containsKey(word))
+					wordLinks.get(word).add(documents.get(docNo));
+				else
+				{
+					Set<Page> docSet = new HashSet<>();
+					docSet.add(documents.get(docNo));
+					wordLinks.put(word, docSet);
+				}
+			}
+			docNo++;
+		}
+		
+		for(int i = 0; i < 22; i++)
+		{
+			reset(documents);
+			for(Map.Entry<Integer, Page> e : documents.entrySet())
+			{
+				Page p = e.getValue();
+				Set<Page> links = new HashSet<>();
+				for(String s : p.words)
+					for(Page l : wordLinks.get(s))
+						links.add(l);
+				links.remove(p);
+				p.calculateScore(links);
+			}
+		}
+		
+		
+		Page[] pgs = new Page[documents.size()];
+		pgs = documents.values().toArray(pgs);
+		Arrays.sort(pgs);
+		int x = 0;
+		for(Page p : pgs)
+		{
+			System.out.println(p.docNo + " -> " + p.newRankScore);
+			if(x++ > 10)
+				break;
+		}
+	}
+	
+	public static void reset(Map<Integer, Page> documents)
+	{
+		for(Map.Entry<Integer, Page> e : documents.entrySet())
+		{
+			Page p = e.getValue();
+			p.oldRankScore = p.newRankScore;
+			p.newRankScore = 1.0;
 		}
 	}
 }
